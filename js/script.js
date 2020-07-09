a60835149d138ebbd5a98678437e2b413121512d
Shad Decker <DeckerShadN@JohnDeere.com>
2020-07-08 19:47:08 -0500

    added mouseover event

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ class Cell {
   constructor(cellNumber) {
     this.cellNumber = cellNumber;
     this.clicked = false;
-    this.color = "tan";
+    this.color = "white";
   }
 }
 
@@ -59,167 +59,44 @@ function displayWinner(winningPlayer) {
   winnerBox.style.display = "block";
 }
 
+function squareClickHandler() {
+  let currentCell = event.target.id.substring(7);
+  document.querySelector(`#square_${currentCell}`).style.background = myGame.nextColor;
+  myGame.changeColor(currentCell);
+  if (myGame.compare(myGame.player1Moves)) {
+    displayWinner("Player 1");
+  } else if (myGame.compare(myGame.player2Moves)) {
+    displayWinner("Player 2");
+  }
+}
+
+function squareMouseOverHandler() {
+  let currentCell = event.target.id.substring(7);
+  document.querySelector(`#square_${currentCell}`).style.background = myGame.nextColor;
+}
+
+function squareMouseOutHandler() {
+  let currentCell = event.target.id.substring(7);
+  document.querySelector(`#square_${currentCell}`).style.background = "white";
+}
+
 let square1 = document.querySelector("#square_1");
-square1.addEventListener("click", () => {
-  let currentCell = this;
-  console.log(currentCell);
-  if (!myCell1.clicked) {
-    document.querySelector("#square_1").style.background = myGame.nextColor;
-    myCell1.clicked = true;
-    myCell1.color = myGame.nextColor;
-    myGame.changeColor(1);
-    if (myGame.compare(myGame.player1Moves)) {
-      displayWinner("Player 1");
-    } else if (myGame.compare(myGame.player2Moves)) {
-      displayWinner("Player 2");
-    }
-  } else {
-    console.log(`cell already clicked and is ${myCell1.color}`);
-  }
-});
-
 let square2 = document.querySelector("#square_2");
-square2.addEventListener("click", () => {
-  if (!myCell2.clicked) {
-    document.querySelector("#square_2").style.background = myGame.nextColor;
-    myCell2.clicked = true;
-    myCell2.color = myGame.nextColor;
-    myGame.changeColor(2);
-    if (myGame.compare(myGame.player1Moves)) {
-      displayWinner("Player 1");
-    } else if (myGame.compare(myGame.player2Moves)) {
-      displayWinner("Player 2");
-    }
-  } else {
-    console.log(`cell already clicked and is ${myCell2.color}`);
-  }
-});
-
 let square3 = document.querySelector("#square_3");
-square3.addEventListener("click", () => {
-  if (!myCell3.clicked) {
-    document.querySelector("#square_3").style.background = myGame.nextColor;
-    myCell3.clicked = true;
-    myCell3.color = myGame.nextColor;
-    myGame.changeColor(3);
-    if (myGame.compare(myGame.player1Moves)) {
-      displayWinner("Player 1");
-    } else if (myGame.compare(myGame.player2Moves)) {
-      displayWinner("Player 2");
-    }
-  } else {
-    console.log(`cell already clicked and is ${myCell3.color}`);
-  }
-});
-
 let square4 = document.querySelector("#square_4");
-square4.addEventListener("click", () => {
-  if (!myCell4.clicked) {
-    document.querySelector("#square_4").style.background = myGame.nextColor;
-    myCell4.clicked = true;
-    myCell4.color = myGame.nextColor;
-    myGame.changeColor(4);
-    if (myGame.compare(myGame.player1Moves)) {
-      displayWinner("Player 1");
-    } else if (myGame.compare(myGame.player2Moves)) {
-      displayWinner("Player 2");
-    }
-  } else {
-    console.log(`cell already clicked and is ${myCell4.color}`);
-  }
-});
-
 let square5 = document.querySelector("#square_5");
-square5.addEventListener("click", () => {
-  if (!myCell5.clicked) {
-    document.querySelector("#square_5").style.background = myGame.nextColor;
-    myCell5.clicked = true;
-    myCell5.color = myGame.nextColor;
-    myGame.changeColor(5);
-    if (myGame.compare(myGame.player1Moves)) {
-      displayWinner("Player 1");
-    } else if (myGame.compare(myGame.player2Moves)) {
-      displayWinner("Player 2");
-    }
-  } else {
-    console.log(`cell already clicked and is ${myCell5.color}`);
-  }
-});
-
 let square6 = document.querySelector("#square_6");
-square6.addEventListener("click", () => {
-  if (!myCell6.clicked) {
-    document.querySelector("#square_6").style.background = myGame.nextColor;
-    myCell6.clicked = true;
-    myCell6.color = myGame.nextColor;
-    myGame.changeColor(6);
-    if (myGame.compare(myGame.player1Moves)) {
-      displayWinner("Player 1");
-    } else if (myGame.compare(myGame.player2Moves)) {
-      displayWinner("Player 2");
-    }
-  } else {
-    console.log(`cell already clicked and is ${myCell6.color}`);
-  }
-});
-
 let square7 = document.querySelector("#square_7");
-square7.addEventListener("click", () => {
-  if (!myCell7.clicked) {
-    document.querySelector("#square_7").style.background = myGame.nextColor;
-    myCell7.clicked = true;
-    myCell7.color = myGame.nextColor;
-    myGame.changeColor(7);
-    if (myGame.compare(myGame.player1Moves)) {
-      displayWinner("Player 1");
-    } else if (myGame.compare(myGame.player2Moves)) {
-      displayWinner("Player 2");
-    }
-  } else {
-    console.log(`cell already clicked and is ${myCell7.color}`);
-  }
-});
-
 let square8 = document.querySelector("#square_8");
-square8.addEventListener("click", () => {
-  if (!myCell8.clicked) {
-    document.querySelector("#square_8").style.background = myGame.nextColor;
-    myCell8.clicked = true;
-    myCell8.color = myGame.nextColor;
-    myGame.changeColor(8);
-    if (myGame.compare(myGame.player1Moves)) {
-      displayWinner("Player 1");
-    } else if (myGame.compare(myGame.player2Moves)) {
-      displayWinner("Player 2");
-    }
-  } else {
-    console.log(`cell already clicked and is ${myCell8.color}`);
-  }
-});
-
 let square9 = document.querySelector("#square_9");
-square9.addEventListener("click", () => {
-  if (!myCell9.clicked) {
-    document.querySelector("#square_9").style.background = myGame.nextColor;
-    myCell9.clicked = true;
-    myCell9.color = myGame.nextColor;
-    myGame.changeColor(9);
-    if (myGame.compare(myGame.player1Moves)) {
-      displayWinner("Player 1");
-    } else if (myGame.compare(myGame.player2Moves)) {
-      displayWinner("Player 2");
-    }
-  } else {
-    console.log(`cell already clicked and is ${myCell9.color}`);
-  }
-});
 
 let startGameButton = document.querySelector("#StartGame");
 startGameButton.addEventListener("click", () => {
   let squares = document.querySelectorAll(".square");
   squares.forEach((square) => {
-    square.style.background = "tan";
+    square.style.background = "white";
   });
+
   myGame.player1Moves = [];
   myGame.player2Moves = [];
   myGame.nextColor = "red";
@@ -232,4 +109,31 @@ startGameButton.addEventListener("click", () => {
   myCell7.clicked = false;
   myCell8.clicked = false;
   myCell9.clicked = false;
+  square1.addEventListener("click", squareClickHandler, { once: true });
+  square1.addEventListener("mouseover", squareMouseOverHandler);
+  square1.addEventListener("mouseout", squareMouseOutHandler);
+  square2.addEventListener("click", squareClickHandler, { once: true });
+  square2.addEventListener("mouseover", squareMouseOverHandler);
+  square2.addEventListener("mouseout", squareMouseOutHandler);
+  square3.addEventListener("click", squareClickHandler, { once: true });
+  square3.addEventListener("mouseover", squareMouseOverHandler);
+  square3.addEventListener("mouseout", squareMouseOutHandler);
+  square4.addEventListener("click", squareClickHandler, { once: true });
+  square4.addEventListener("mouseover", squareMouseOverHandler);
+  square4.addEventListener("mouseout", squareMouseOutHandler);
+  square5.addEventListener("click", squareClickHandler, { once: true });
+  square5.addEventListener("mouseover", squareMouseOverHandler);
+  square5.addEventListener("mouseout", squareMouseOutHandler);
+  square6.addEventListener("click", squareClickHandler, { once: true });
+  square6.addEventListener("mouseover", squareMouseOverHandler);
+  square6.addEventListener("mouseout", squareMouseOutHandler);
+  square7.addEventListener("click", squareClickHandler, { once: true });
+  square7.addEventListener("mouseover", squareMouseOverHandler);
+  square7.addEventListener("mouseout", squareMouseOutHandler);
+  square8.addEventListener("click", squareClickHandler, { once: true });
+  square8.addEventListener("mouseover", squareMouseOverHandler);
+  square8.addEventListener("mouseout", squareMouseOutHandler);
+  square9.addEventListener("click", squareClickHandler, { once: true });
+  square9.addEventListener("mouseover", squareMouseOverHandler);
+  square9.addEventListener("mouseout", squareMouseOutHandler);
 });
